cb38a96c3dcbc333b75481685bd6daad8c53f017
Igor <121062670+Who-Is-Igor-Bro@users.noreply.github.com>
2025-05-22 10:38:25 -0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,465 @@
+// products.json
+var productsData = [
+  {
+    "id": 1,
+    "name": "BIOMAX 1.000 IMCU/mL",
+    "price": 50.00,
+    "image": "images/quimosina.jpg",
+    "description": "Solução líquida pura de quimosina 100% para todos os tipos de queijos. Dosagem: 30 a 40 mL / 1.000 L de leite. Embalagem: 1 L, 5 L."
+  },
+  {
+    "id": 2,
+    "name": "BIOMAX-PLUS 600 IMCU/mL",
+    "price": 45.00,
+    "image": "images/quimosina.jpg",
+    "description": "Solução líquida pura de quimosina 100% para todos os tipos de queijos. Dosagem: 40 a 60 mL / 1.000 L de leite. Embalagem: 1 L, 5 L."
+  },
+  {
+    "id": 3,
+    "name": "BIOMAX 600 IMCU/mL",
+    "price": 40.00,
+    "image": "images/quimosina.jpg",
+    "description": "Solução líquida pura de quimosina 100% para todos os tipos de queijos. Dosagem: 40 a 60 mL / 1.000 L de leite. Embalagem: 1 L, 5 L."
+  },
+  {
+    "id": 4,
+    "name": "BTECH-PLUS 600 IMCU/mL",
+    "price": 42.00,
+    "image": "images/quimosina.jpg",
+    "description": "Solução líquida pura de quimosina 100% para todos os tipos de queijos. Dosagem: 40 a 60 mL / 1.000 L de leite. Embalagem: 1 L, 5 L."
+  },
+  {
+    "id": 5,
+    "name": "MICROFILTER 1.200 IMCU/L",
+    "price": 55.00,
+    "image": "images/coagulante.jpg",
+    "description": "Coagulante líquido Rhizomucor mihei para todos os tipos de queijos. Dosagem: 30 a 40 mL / 1.000 L de leite. Embalagem: 1 L, 5 L."
+  },
+  {
+    "id": 6,
+    "name": "HIPERLAT 1.200 IMCU/L",
+    "price": 54.00,
+    "image": "images/coagulante.jpg",
+    "description": "Coagulante líquido Rhizomucor mihei para todos os tipos de queijos. Dosagem: 30 a 40 mL / 1.000 L de leite. Embalagem: 1 L, 5 L."
+  },
+  {
+    "id": 7,
+    "name": "MICROFILTER 900 IMCU/L",
+    "price": 48.00,
+    "image": "images/coagulante.jpg",
+    "description": "Coagulante líquido Rhizomucor mihei para todos os tipos de queijos. Dosagem: 40 a 60 mL / 1.000 L de leite. Embalagem: 1 L, 5 L."
+  },
+  {
+    "id": 8,
+    "name": "MICROFILTER 750 IMCU/L",
+    "price": 45.00,
+    "image": "images/coagulante.jpg",
+    "description": "Coagulante líquido Rhizomucor mihei para todos os tipos de queijos. Dosagem: 60 a 80 mL / 1.000 L de leite. Embalagem: 1 L, 5 L."
+  },
+  {
+    "id": 9,
+    "name": "COALHOBRASIL 750 IMCU/L",
+    "price": 44.00,
+    "image": "images/coagulante.jpg",
+    "description": "Coagulante líquido Rhizomucor mihei para todos os tipos de queijos. Dosagem: 60 a 80 mL / 1.000 L de leite. Embalagem: 1 L, 5 L."
+  },
+  {
+    "id": 10,
+    "name": "BOM PASTOR 750 IMCU/L",
+    "price": 44.50,
+    "image": "images/coagulante.jpg",
+    "description": "Coagulante líquido Rhizomucor mihei para todos os tipos de queijos. Dosagem: 60 a 80 mL / 1.000 L de leite. Embalagem: 1 L, 5 L."
+  },
+  {
+    "id": 11,
+    "name": "PASTORINHA 750 IMCU/L",
+    "price": 44.50,
+    "image": "images/coagulante.jpg",
+    "description": "Coagulante líquido Rhizomucor mihei para todos os tipos de queijos. Dosagem: 60 a 80 mL / 1.000 L de leite. Embalagem: 1 L, 5 L."
+  },
+  {
+    "id": 12,
+    "name": "BTECH 1.50 IMCU/mL",
+    "price": 60.00,
+    "image": "images/coagulante.jpg",
+    "description": "Coagulante líquido para todos os tipos de queijos. Dosagem: 200 a 300 mL / 1.000 L de leite. Embalagem: 1 L, 5 L, 20 L, 50 L."
+  },
+  {
+    "id": 13,
+    "name": "MICROFILTER 3.300 IMCU/g",
+    "price": 70.00,
+    "image": "images/coagulante_po.jpg",
+    "description": "Coagulante em pó Rhizomucor mihei para todos os tipos de queijos. Dosagem: 15 a 20 g / 1.000 L de leite. Embalagem: 500 g, 3 kg."
+  },
+  {
+    "id": 14,
+    "name": "MICROFILTER 1.900 IMCU/g",
+    "price": 65.00,
+    "image": "images/coagulante_po.jpg",
+    "description": "Coagulante em pó Rhizomucor mihei para todos os tipos de queijos. Dosagem: 25 a 30 g / 1.000 L de leite. Embalagem: 500 g, 3 kg."
+  },
+  {
+    "id": 15,
+    "name": "MICROFILTER 500 IMCU/g",
+    "price": 60.00,
+    "image": "images/coagulante_po.jpg",
+    "description": "Coagulante em pó Rhizomucor mihei para todos os tipos de queijos. Dosagem: 5 a 10 g / 100 L de leite. Embalagem: 50 g, 2 kg."
+  },
+  {
+    "id": 16,
+    "name": "BOM PASTOR MAX 1:20.000",
+    "price": 55.00,
+    "image": "images/coalho_liquido.jpg",
+    "description": "Enzima coagulante líquida para todos os tipos de queijos. Dosagem: 100 a 150 mL / 1.000 L de leite. Embalagem: 1 L, 5 L, 20 L, 50 L."
+  },
+  {
+    "id": 17,
+    "name": "BOM PASTOR 1:12.000",
+    "price": 50.00,
+    "image": "images/coalho_liquido.jpg",
+    "description": "Enzima coagulante líquida para todos os tipos de queijos. Dosagem: 150 a 200 mL / 1.000 L de leite. Embalagem: 1 L, 5 L, 20 L, 50 L."
+  },
+  {
+    "id": 18,
+    "name": "BOM PASTOR 1:10.000",
+    "price": 48.00,
+    "image": "images/coalho_liquido.jpg",
+    "description": "Enzima coagulante líquida para todos os tipos de queijos. Dosagem: 200 a 300 mL / 1.000 L de leite. Embalagem: 1 L, 5 L, 20 L, 50 L."
+  },
+  {
+    "id": 19,
+    "name": "COALHOBRASIL 1:10.000",
+    "price": 48.00,
+    "image": "images/coalho_liquido.jpg",
+    "description": "Enzima coagulante líquida para todos os tipos de queijos. Dosagem: 200 a 300 mL / 1.000 L de leite. Embalagem: 1 L, 5 L, 20 L, 50 L."
+  },
+  {
+    "id": 20,
+    "name": "PASTORINHA 1:10.000",
+    "price": 48.00,
+    "image": "images/coalho_liquido.jpg",
+    "description": "Enzima coagulante líquida para todos os tipos de queijos. Dosagem: 200 a 300 mL / 1.000 L de leite. Embalagem: 1 L, 5 L, 20 L, 50 L."
+  },
+  {
+    "id": 21,
+    "name": "BOM PASTOR 1:10.000 (Pó)",
+    "price": 65.00,
+    "image": "images/coalho_po.jpg",
+    "description": "Enzima coagulante em pó para todos os tipos de queijos. Dosagem: 3 g / 150 a 200 L de leite. Embalagem: 500 g, 3 kg."
+  },
+  {
+    "id": 22,
+    "name": "BOM PASTOR 1:30.000 (Pó)",
+    "price": 70.00,
+    "image": "images/coalho_po.jpg",
+    "description": "Enzima coagulante em pó para todos os tipos de queijos. Dosagem: 25 a 30 g / 1.000 L de leite. Embalagem: 500 g, 3 kg."
+  },
+  {
+    "id": 23,
+    "name": "PASTORINHA 1:80.000 (Pó)",
+    "price": 75.00,
+    "image": "images/coalho_po.jpg",
+    "description": "Enzima coagulante em pó para todos os tipos de queijos. Dosagem: 25 a 30 g / 1.000 L de leite. Embalagem: 50 g, 3 kg."
+  },
+  {
+    "id": 24,
+    "name": "COALHOBRASIL 1:80.000 (Pó)",
+    "price": 75.00,
+    "image": "images/coalho_po.jpg",
+    "description": "Enzima coagulante em pó para todos os tipos de queijos. Dosagem: 25 a 30 g / 1.000 L de leite. Embalagem: 50 g, 3 kg."
+  },
+  {
+    "id": 25,
+    "name": "COALHOBRASIL 1:40.000 (Pó)",
+    "price": 70.00,
+    "image": "images/coalho_po.jpg",
+    "description": "Enzima coagulante em pó para todos os tipos de queijos. Dosagem: 50 g / 3 kg. Embalagem: 50 g, 2 kg."
+  },
+  {
+    "id": 26,
+    "name": "YO-1 100 U",
+    "price": 30.00,
+    "image": "images/fermento_iogurte.jpg",
+    "description": "Fermento para iogurte com Streptococcus thermophilus e Lactobacillus delbrueckii subsp. bulgaricus. Dosagem: 10.000 L de leite. Embalagem: 100 U."
+  },
+  {
+    "id": 27,
+    "name": "YO-2 100 U",
+    "price": 32.00,
+    "image": "images/fermento_iogurte.jpg",
+    "description": "Fermento para iogurte com Streptococcus thermophilus e Lactobacillus delbrueckii subsp. bulgaricus. Dosagem: 10.000 L de leite. Embalagem: 100 U."
+  },
+  {
+    "id": 28,
+    "name": "YO-3 100 U",
+    "price": 34.00,
+    "image": "images/fermento_iogurte.jpg",
+    "description": "Fermento para iogurte com Streptococcus thermophilus e Lactobacillus delbrueckii subsp. bulgaricus. Dosagem: 10.000 L de leite. Embalagem: 100 U."
+  },
+  {
+    "id": 29,
+    "name": "DI 50 U",
+    "price": 40.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Streptococcus salivarius subsp. thermophilus, Lactobacillus delbrueckii subsp. bulgaricus, Lactobacillus helveticus. Dosagem: 5.000 L de leite. Aplicação: Mussarela, Provolone, Parmesão, Parmigiano, Grana Padano."
+  },
+  {
+    "id": 30,
+    "name": "MTE 50 U",
+    "price": 42.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Lactobacillus helveticus, Streptococcus salivarius subsp. thermophilus. Dosagem: 10.000 L de leite. Aplicação: Mussarela, Provolone, Parmesão, Latteria, Asiago, Caciotta, Kashkaval."
+  },
+  {
+    "id": 31,
+    "name": "NC 50 U",
+    "price": 41.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Streptococcus salivarius subsp. thermophilus, Lactobacillus delbrueckii subsp. bulgaricus. Dosagem: 10.000 L de leite. Aplicação: Mussarela, Mussarela de Búfala, Provolone, Latteria, Asiago."
+  },
+  {
+    "id": 32,
+    "name": "QGE 50 U",
+    "price": 40.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Streptococcus salivarius subsp. thermophilus. Dosagem: 10.000 L de leite. Aplicação: Mussarela, Caciotta, Scamorza."
+  },
+  {
+    "id": 33,
+    "name": "MVD 50 U",
+    "price": 43.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Streptococcus salivarius subsp. thermophilus. Dosagem: 10.000 L de leite. Aplicação: Mussarela, Caciotta, Scamorza."
+  },
+  {
+    "id": 34,
+    "name": "TMLD 15 U",
+    "price": 38.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Streptococcus salivarius subsp. thermophilus, Lactobacillus casei, Lactococcus lactis subsp. lactis, Lactococcus lactis subsp. cremoris, Lactobacillus delbrueckii subsp. bulgaricus, Lactobacillus helveticus. Dosagem: 5.000 L de leite. Aplicação: Prato, Camembert, Gouda, Edam, Minas Padrão, Reino, Pecorino Sardo."
+  },
+  {
+    "id": 35,
+    "name": "MSTD 15 U",
+    "price": 37.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Lactococcus lactis subsp. lactis, Lactococcus lactis subsp. cremoris, Streptococcus salivarius subsp. thermophilus. Dosagem: 5.000 L de leite. Aplicação: Prato, Camembert, Gouda, Edam, Minas Padrão, Reino, Feta."
+  },
+  {
+    "id": 36,
+    "name": "UNB 50 U",
+    "price": 39.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Lactococcus lactis subsp. lactis, Lactococcus lactis subsp. cremoris. Dosagem: 5.000 L de leite. Aplicação: Prato, Minas Padrão, Reino, Cheddar, Edam, Manchego, Requeijão, Quark, Cottage, Manteiga."
+  },
+  {
+    "id": 37,
+    "name": "UNC 50 U",
+    "price": 40.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Lactococcus lactis subsp. lactis, Lactococcus lactis subsp. cremoris, Streptococcus thermophilus subsp. thermophilus. Dosagem: 5.000 L de leite. Aplicação: Prato, Reino, Cheddar, Feta, Camembert, Gouda, Edam, Manchego."
+  },
+  {
+    "id": 38,
+    "name": "QDT 50 U",
+    "price": 41.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Streptococcus salivarius subsp. thermophilus, Lactobacillus casei, Lactococcus lactis subsp. lactis, Lactococcus lactis subsp. cremoris, Lactobacillus helveticus. Dosagem: 10.000 L de leite. Aplicação: Prato, Reino, Cheddar, Edam, Manchego, Pecorino Sardo."
+  },
+  {
+    "id": 39,
+    "name": "NF 50 U",
+    "price": 42.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Lactococcus lactis subsp. lactis, Lactococcus lactis subsp. cremoris, Lactococcus lactis subsp. diacetylactis, Leuconostoc mesenteroides subsp. cremoris. Dosagem: 5.000 L de leite. Aplicação: Prato, Estepe, Gouda, Cheddar, Edam, Gorgonzola."
+  },
+  {
+    "id": 40,
+    "name": "PLH 50 U",
+    "price": 43.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Lactobacillus helveticus, Streptococcus salivarius subsp. thermophilus. Dosagem: 5.000 L de leite. Aplicação: Parmesão, Kashkaval."
+  },
+  {
+    "id": 41,
+    "name": "QQS 50 U",
+    "price": 44.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Propionibacterium freudenreichii subsp. shermanii. Dosagem: 5.000 L de leite. Aplicação: Emmental, Estepe, Edamer, Gruyere, Gouda, Leerdammer."
+  },
+  {
+    "id": 42,
+    "name": "PC-1 10 U",
+    "price": 45.00,
+    "image": "images/fermento_queijo.jpg",
+    "description": "Fermento com Penicillium candidum. Dosagem: 1.000 L de leite. Aplicação: Queijos de mofo branco como Camembert, Brie, Saga."
+  },
+  {
+    "id": 43,
+    "name": "CLORETO DE CÁLCIO",
+    "price": 25.00,
+    "image": "images/estabilizante.jpg",
+    "description": "Solução aquosa que melhora a coagulação do leite cru e reconstitui o cálcio perdido no leite pasteurizado. Dosagem: 200 a 300 mL / 1.000 L de leite. Embalagem: 20 L, 5 L, 25 g."
+  },
+  {
+    "id": 44,
+    "name": "PIMARICINA",
+    "price": 80.00,
+    "image": "images/conservante.jpg",
+    "description": "Antifúngico natural para superfícies de queijos. Dosagem: 2-10 g / L de solução salina 10%. Embalagem: 1 kg, 500 g, 200 g."
+  },
+  {
+    "id": 45,
+    "name": "NISINA",
+    "price": 85.00,
+    "image": "images/conservante.jpg",
+    "description": "Antimicrobiano natural para fabricação de queijos, produtos cárneos e superfícies de embutidos. Dosagem: 12,5 mg / kg (dose máxima). Embalagem: 1 kg, 500 g, 200 g."
+  },
+  {
+    "id": 46,
+    "name": "NITRATO DE SÓDIO",
+    "price": 30.00,
+    "image": "images/conservante.jpg",
+    "description": "Conservante natural que previne o estufamento de queijos. Dosagem: 150 a 200 g / 1.000 L de leite. Embalagem: 25 kg."
+  },
+  {
+    "id": 47,
+    "name": "LACTASE BTECH 5000 NLU",
+    "price": 90.00,
+    "image": "images/enzima.jpg",
+    "description": "Enzima para eliminação controlada da lactose no leite. Dosagem: 800 mL / 1.000 L de leite no silo ou 400 mL / 1.000 L no queijomatic. Embalagem: 20 kg, 10 kg, 5 kg, 1 kg."
+  },
+  {
+    "id": 48,
+    "name": "LIPASE BTECH L",
+    "price": 75.00,
+    "image": "images/enzima.jpg",
+    "description": "Enzima para intensificação de sabor em Parmesão e Provolone. Dosagem: 40 a 60 g / 1.000 L de leite. Embalagem: 1 kg, 500 g."
+  },
+  {
+    "id": 49,
+    "name": "LISOZIMA",
+    "price": 70.00,
+    "image": "images/enzima.jpg",
+    "description": "Enzima para inibição de bactérias formadoras de esporos em lácteos, carnes e vinhos. Dosagem: 20 a 25 g / 1.000 L de leite. Embalagem: 500 g."
+  },
+  {
+    "id": 50,
+    "name": "CLOROFILA",
+    "price": 35.00,
+    "image": "images/corante.jpg",
+    "description": "Corante natural para branqueamento do leite e do soro. Dosagem: 5 a 20 mL / 100 L de leite ou soro. Embalagem: 20 L, 5 L."
+  },
+  {
+    "id": 51,
+    "name": "URUCUM",
+    "price": 35.00,
+    "image": "images/corante.jpg",
+    "description": "Corante para coloração de queijos, salsichas e polpas de frutas. Dosagem: 5 a 20 mL / 100 L de leite ou soro. Embalagem: 20 L, 5 L."
+  },
+  {
+    "id": 52,
+    "name": "BIOMIC",
+    "price": 60.00,
+    "image": "images/bioprotetor.jpg",
+    "description": "Revestimento para queijos com pimaricina incolor. Inibe crescimento de microrganismos e reduz perda de umidade. Aplicação manual. Embalagem: 20 L, 5 L, 1 L."
+  },
+  {
+    "id": 53,
+    "name": "PROPLAST",
+    "price": 65.00,
+    "image": "images/bioprotetor.jpg",
+    "description": "Revestimento para queijos incolor, vermelho, preto ou amarelo. Recomenda-se 3 aplicações. Embalagem: 5 L, 1 L."
+  },
+  {
+    "id": 54,
+    "name": "FUMAÇA LÍQUIDA",
+    "price": 50.00,
+    "image": "images/aroma.jpg",
+    "description": "Aromatizante para Provolone. Dosagem: Dissolver 1 L de fumaça em 10 L de água, acrescentar 10% de sal e 10% de álcool. Embalagem: 5 L, 1 L."
+  },
+  {
+    "id": 55,
+    "name": "DIÓXIDO DE CLORO",
+    "price": 45.00,
+    "image": "images/tratamento.jpg",
+    "description": "Tratamento de água, alimentos e superfícies por pulverização ou imersão. Consultar ficha técnica. Embalagem: 5 L, 1 L."
+  },
+  {
+    "id": 56,
+    "name": "BTECH-TEST R",
+    "price": 20.00,
+    "image": "images/teste_rapido.jpg",
+    "description": "Teste rápido para detecção de β-Lactâmicos e Tetraciclinas em leite."
+  },
+  {
+    "id": 57,
+    "name": "BTECH-TEST BTC",
+    "price": 22.00,
+    "image": "images/teste_rapido.jpg",
+    "description": "Teste rápido para detecção de β-Lactâmicos, Tetraciclinas e Cefalexina em leite."
+  },
+  {
+    "id": 58,
+    "name": "BTECH-TEST BTG",
+    "price": 23.00,
+    "image": "images/teste_rapido.jpg",
+    "description": "Teste rápido para detecção de β-Lactâmicos, Tetraciclinas e Gentamicina em leite."
+  },
+  {
+    "id": 59,
+    "name": "BTECH-TEST S",
+    "price": 24.00,
+    "image": "images/teste_rapido.jpg",
+    "description": "Teste rápido para detecção de β-Lactâmicos, Tetraciclinas e Sulfonamidas em leite."
+  },
+  {
+    "id": 60,
+    "name": "BTECH-TEST Q",
+    "price": 25.00,
+    "image": "images/teste_rapido.jpg",
+    "description": "Teste rápido para detecção de β-Lactâmicos, Tetraciclinas, Sulfonamidas e Quinolonas em leite."
+  },
+  {
+    "id": 61,
+    "name": "BTECH-TEST QMLE",
+    "price": 26.00,
+    "image": "images/teste_rapido.jpg",
+    "description": "Teste rápido para detecção de Quinolonas, Macrolídeos, Lincomicina e Eritromicina em leite."
+  },
+  {
+    "id": 62,
+    "name": "BTECH-TEST MAC",
+    "price": 22.00,
+    "image": "images/teste_rapido.jpg",
+    "description": "Teste rápido para detecção de Macrolídeos em leite."
+  },
+  {
+    "id": 63,
+    "name": "BTECH-TEST G",
+    "price": 21.00,
+    "image": "images/teste_rapido.jpg",
+    "description": "Teste rápido para detecção de Gentamicina em leite."
+  },
+  {
+    "id": 64,
+    "name": "BTECH-TEST GNKS",
+    "price": 27.00,
+    "image": "images/teste_rapido.jpg",
+    "description": "Teste rápido para detecção de Gentamicina, Neomicina, Canamicina e Estreptomicina em leite."
+  },
+  {
+    "id": 65,
+    "name": "TESTE PERÓXIDO",
+    "price": 15.00,
+    "image": "images/teste_rapido.jpg",
+    "description": "Tiras reagentes para detecção de peróxido em leite, leite em pó, água, produtos lácteos e controles residuais. Embalagem: Tubo c/ 50 tiras."
+  },
+  {
+    "id": 66,
+    "name": "TESTE FOSFATASE",
+    "price": 16.00,
+    "image": "images/teste_rapido.jpg",
+    "description": "Tiras reagentes para detecção semiquantitativa de fosfatase (ALP) em leite pasteurizado."
+  }
+];
